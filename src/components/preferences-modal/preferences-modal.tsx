0838dd6648116ceb5263300d1f07a8e5e077d940
Ed <edwoulfe@gmail.com>
2024-04-22 22:05:01 -0400

    preferences modal styling and radio buttons implemented, on click will change the local storage and context of the user preferences.

--- src/components/preferences-modal/preferences-modal.tsx
+++ src/components/preferences-modal/preferences-modal.tsx
@@ -1,92 +1,89 @@
-import { FormEvent, useContext } from "react";
+import { ChangeEvent, useContext } from "react";
 import "./preferences-modal.scss";
 import { PreferencesContext } from "../../context/PreferencesContext";
+import PreferencesInterface from "../../interfaces/preferences";
 
 export function PreferencesModal() {
   const { preferences, setPreferences } = useContext(PreferencesContext);
 
-  //   function createRadioButtonGroup(numOfButtons: number, dataType: string, currentPreferences: PreferencesInterface) {
-  //     const selectedNumber = currentPreferences[dataType as keyof PreferencesInterface];
-  //     let arr = [];
+  function createRadioButtonGroup(numOfButtons: number, dataType: string, currentPreferences: PreferencesInterface) {
+    const selectedNumber = currentPreferences[dataType as keyof PreferencesInterface];
+    const radioButtonGroupJSX = [];
 
-  //     for (let i = 1; i <= numOfButtons; i++) {
-  //       if (i === selectedNumber) {
-  //         arr.push(
-  //           <>
-  //             {" "}
-  //             <label htmlFor={`${dataType}-${i}__radio`}>{i}</label>
-  //             <input type="radio" id={`${dataType}-${i}__radio`} name={dataType} value={i} checked />
-  //           </>
-  //         );
-  //       } else {
-  //         arr.push(
-  //           <>
-  //             {" "}
-  //             <label htmlFor={`${dataType}-${i}__radio`}>{i}</label>
-  //             <input type="radio" id={`${dataType}-${i}__radio`} name={dataType} value={i} />
-  //           </>
-  //         );
-  //       }
-  //     }
+    for (let i = 1; i <= numOfButtons; i++) {
+      if (i == selectedNumber) {
+        radioButtonGroupJSX.push(
+          <li className="radio__list-item" key={dataType + "-button-" + i}>
+            <label htmlFor={`${dataType}-${i}__radio`}>{i}</label>
+            <input
+              type="radio"
+              id={`${dataType}-${i}__radio`}
+              name={dataType}
+              key={i}
+              onChange={(event) => {
+                setPreferences({ ...preferences, [event.target.name]: event.target.value });
+              }}
+              value={i}
+              checked
+            />
+          </li>
+        );
+      } else {
+        radioButtonGroupJSX.push(
+          <li className="radio__list-item" key={dataType + "-button-" + i}>
+            <label htmlFor={`${dataType}-${i}__radio`}>{i}</label>
+            <input
+              type="radio"
+              id={`${dataType}-${i}__radio`}
+              name={dataType}
+              onChange={(event) => {
+                setPreferences({ ...preferences, [event.target.name]: event.target.value });
+              }}
+              value={i}
+            />
+          </li>
+        );
+      }
+    }
 
-  //     return arr.map((html) => {
-  //       return html;
-  //     });
-  //   }
-
-  function changePreferences(formData: FormEvent<HTMLFormElement>) {
-    // setPreferences({ ...preferences, [property]: value });
-    console.log("submitted");
-
-    console.log(formData);
+    return radioButtonGroupJSX.map((radioButton) => {
+      return radioButton;
+    });
   }
 
   return (
-    <div className="preferences__container">
+    <div className="modal__container">
       <h2>Preferences</h2>
-
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          changePreferences(event);
-        }}>
-        <fieldset>
-          <legend>
-            Give each category a number rating, based on importance to you. 1 being lowest priority 5 being of highest
-            priority.
-          </legend>
-          <ul>
-            <li>
-              <label>Cost of Living</label>
-            </li>
-            <li>
-              <label htmlFor="median-income__radio">Median Income</label>
-            </li>
-            <li>
-              <label htmlFor="median-house-value__radio">Median House Value</label>
-
-              <input type="radio" id="median-house-value__radio" value="house value" />
-            </li>
-            <li>
-              <label htmlFor="median-age__radio">Median Age</label>
-
-              <input type="radio" id="median-age__radio" value="age" />
-            </li>
-            <li>
-              <label htmlFor="population__radio">Larger Population</label>
-
-              <input type="radio" id="population__radio" value="population" />
-            </li>
-            <li>
-              <label htmlFor="education__radio">Highest Education Achieved</label>
-
-              <input type="radio" id="education__radio" value="education" />
-            </li>
-          </ul>
-
-          <button value="submit" />
-        </fieldset>
-      </form>
+      <p className="modal__description">
+        Give each category a number rating, based on importance to you. 1 being lowest priority 5 being of highest
+        priority.
+      </p>
+      <div className="preferences__container">
+        <span className="preference">
+          <p className="preference__name">Cost of Living</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "costOfLiving", preferences)}</ul>
+        </span>
+        <span className="preference">
+          <p className="preference__name">Median Income</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "medianIncome", preferences)}</ul>
+        </span>
+        <span className="preference">
+          <p className="preference__name">Median House Value</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "medianHouseValue", preferences)}</ul>
+        </span>
+        <span className="preference">
+          <p className="preference__name">Median Age</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "medianAge", preferences)}</ul>
+        </span>
+        <span className="preference">
+          <p className="preference__name">Larger Population</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "largePopulation", preferences)}</ul>
+        </span>
+        <span className="preference">
+          <p className="preference__name">Highest Education Achieved</p>
+          <ul className="buttons__list">{createRadioButtonGroup(5, "highestEducation", preferences)}</ul>
+        </span>
+      </div>
     </div>
   );
 }
